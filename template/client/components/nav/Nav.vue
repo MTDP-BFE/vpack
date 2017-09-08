f1ce10fe35b29c189279ea99aed80d7734fd9c2c
yangzj1992 <yangzj1992@qq.com>
2017-09-08 14:35:58 +0800

    feat: cq template

--- template/client/components/nav/Nav.vue
+++ template/client/components/nav/Nav.vue
@@ -1,9 +1,3 @@
-<template>
-    <div class="nav">
-
-    </div>
-</template>
-
 <script>
 /**
  *
@@ -53,8 +47,6 @@ export default {
         return createElement(
             'div',
             [
-                'jijij',
-                createElement('h1', '一则头条'),
                 createElement(
                     'el-menu',
                     // 把mode和menu-trigger放在一起会报错不知道什么原因
@@ -69,7 +61,18 @@ export default {
                         }
                     },
                     [
-                        ...renderMenuLists(menuLists, createElement)
+                        <li class="el-submenu">
+                            <div class="el-submenu__title logo">MDBI</div>
+                        </li>,
+                        ...renderMenuLists(menuLists, createElement),
+                        <li class="el-submenu right-list">
+                            <div class="el-submenu__title">
+                                <i class="bfui-icon-book search-icon"></i>
+                                <el-input class="search-input" v-model="input" placeholder="请输入内容"></el-input>
+                                <i class="bfui-icon-question-circle"></i>
+                                <span class="user-name">张三</span>
+                            </div>
+                        </li>
                     ]
                 )
             ]
@@ -83,6 +86,37 @@ export default {
     background: #fff;
     .logo{
         font-size: 24px;
+    }
+    .el-icon-caret-bottom{
+        &:before{
+            content: '\E600';
+        }
+    }
+    .right-list{
+        float: right;
+        i{
+            font-size: 28px;
+            position: relative;
+            top: 4px;
+        }
+        .search-input{
+            margin-left: 10px;
+            input{
+                width: auto;
+                border-radius: 20px;
+            }
+        }
+        .search-input{
+            width: auto;
+        }
+        .bfui-icon-question-circle{
+            margin-left: 30px;
+        }
+        .user-name{
+            position: relative;
+            top: -2px;
+            margin: 0 20px;
+        }
     }
 }
 </style>
