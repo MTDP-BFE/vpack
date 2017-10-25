f02c9756d31de2de3324c3f14c000a888695c3ae
yangzj1992 <yangzj1992@qq.com>
2017-10-25 16:14:11 +0800

    feat: commit & default vuex strict

--- template/client/store/index.js
+++ template/client/store/index.js
@@ -4,13 +4,11 @@ import counter from './modules/counter';
 
 Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production';
-
 const store = new Vuex.Store({
     modules: {
         counter
     },
-    strict: debug
+    strict: false
 });
 
 export default store;
