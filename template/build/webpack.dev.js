46eb598ed658535f2f5bb0a3106766743ed507d7
liudi <liudi10@meituan.com>
2018-04-14 21:39:38 +0800

    Feat：修复css显示声明lang时的打包异常，及依赖包升级

--- template/build/webpack.dev.js
+++ template/build/webpack.dev.js
@@ -1,35 +1,25 @@
 'use strict';
 process.env.NODE_ENV = 'development';
 
+const utils = require('./utils');
 const webpack = require('webpack');
-const base = require('./webpack.base');
-const _ = require('./utils');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base');
 const FriendlyErrors = require('friendly-errors-webpack-plugin');
 
-base.devtool = 'eval-source-map';
-base.plugins.push(
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('development')
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new FriendlyErrors()
-);
+const devWebpackConfig = merge(baseWebpackConfig, {
+    devtool: 'eval-source-map',
+    module : {
+        rules: utils.styleLoaders({sourceMap: true, usePostCSS: true})
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new FriendlyErrors()
+    ]
+})
 
-// push loader for css files
-_.cssProcessors.forEach(processor => {
-    let loaders;
-    if (processor.loader === '') {
-        loaders = ['postcss-loader'];
-    } else {
-        loaders = ['postcss-loader', processor.loader];
-    }
-    base.module.loaders.push(
-        {
-            test: processor.test,
-            loaders: ['style-loader', _.cssLoader].concat(loaders)
-        }
-    );
-});
-
-module.exports = base;
+module.exports = devWebpackConfig;
