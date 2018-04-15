cc8b93c702d3c06699d1800da217bddb5fd0e2b3
liudi <liudi10@meituan.com>
2018-04-15 15:08:26 +0800

    Feat:  fix building error while using lang="postcss" in style tag

--- template/build/utils.js
+++ template/build/utils.js
@@ -3,30 +3,13 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = require('./config');
 
-const _ = module.exports = {};
+exports.outputPath = config.electron ? path.join(__dirname, '../app/dist') : path.join(__dirname, '../dist');
 
-_.cwd = (file) => {
-    return path.join(process.cwd(), file || '');
-};
+exports.outputIndexPath = config.electron ? path.join(__dirname, '../app/dist/index.html') : path.join(__dirname, '../dist/index.html');
 
-_.cssLoader = config.cssModules ? 'css-loader?-autoprefixer&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' : 'css-loader?-autoprefixer';
+exports.target = config.electron ? 'electron-renderer' : 'web';
 
-_.cssProcessors = [
-    { loader: '', test: /\.css$/ },
-    { loader: 'sass-loader?sourceMap', test: /\.scss$/ },
-    { loader: 'less-loader?sourceMap', test: /\.less$/ },
-    { loader: 'stylus-loader?sourceMap', test: /\.styl$/ },
-    { loader: 'sass-loader?indentedSyntax&sourceMap', test: /\.sass$/ },
-];
-
-_.outputPath = config.electron ? path.join(__dirname, '../app/dist') : path.join(__dirname, '../dist');
-
-_.outputIndexPath = config.electron ? path.join(__dirname, '../app/dist/index.html') : path.join(__dirname, '../dist/index.html');
-
-_.target = config.electron ? 'electron-renderer' : 'web';
-
-// https://github.com/egoist/vbuild/blob/master/lib/vue-loaders.js
-_.loadersOptions = () => {
+exports.loadersOptions = () => {
     const isProd = process.env.NODE_ENV === 'production';
 
     function generateLoader(langs) {
@@ -58,3 +41,69 @@ _.loadersOptions = () => {
         }
     };
 };
+
+exports.cssLoaders = function (options) {
+    options = options || {}
+  
+    const cssLoader = {
+      loader: 'css-loader',
+      options: {
+        sourceMap: options.sourceMap
+      }
+    }
+  
+    const postcssLoader = {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: options.sourceMap
+      }
+    }
+  
+    function generateLoaders (loader, loaderOptions) {
+      const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+  
+      if (loader) {
+        loaders.push({
+          loader: loader + '-loader',
+          options: Object.assign({}, loaderOptions, {
+            sourceMap: options.sourceMap
+          })
+        })
+      }
+  
+      if (options.extract) {
+        return ExtractTextPlugin.extract({
+          use: loaders,
+          fallback: 'vue-style-loader'
+        })
+      } else {
+        return ['vue-style-loader'].concat(loaders)
+      }
+    }
+  
+    return {
+      css: generateLoaders(),
+      postcss: generateLoaders(),
+      less: generateLoaders('less'),
+      sass: generateLoaders('sass', { indentedSyntax: true }),
+      scss: generateLoaders('sass'),
+      stylus: generateLoaders('stylus'),
+      styl: generateLoaders('stylus')
+    }
+  }
+  
+// Generate loaders for standalone style files (outside of .vue)
+exports.styleLoaders = function (options) {
+  const output = []
+  const loaders = exports.cssLoaders(options)
+
+  for (const extension in loaders) {
+    const loader = loaders[extension]
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    })
+  }
+
+  return output
+}
